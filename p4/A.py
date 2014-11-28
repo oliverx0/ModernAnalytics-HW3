8aa0f5c30b3f79758b2c7ebb02e494cb4519d788
rachelita <rmayer3@gmail.com>
2014-11-28 18:18:38 -0500

    Finsihed problem 4

---
+++ p4/A.py
@@ -0,0 +1,32 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cross_validation import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from config import *
+from utils import *
+import numpy as np
+import pandas as pd
+import random
+
+movies = load_balanced_movies(MOVIES_DATA, False)
+data = pd.DataFrame(movies)
+
+summaries = data[['summary']]
+summaries['summary'] = summaries['summary'].str.replace('[^\w\s]','')  ## cleans out puncutation and characters
+Y = data[['year']]
+# print summaries.summary
+
+# perform vectorization using Count Vectorizer (counts # of times a word appears)
+vectorizer = CountVectorizer(stop_words='english')
+X = vectorizer.fit_transform(summaries.summary)
+
+print vectorizer.get_feature_names()[0:20]
+
+## splits test and training data
+xtrain, xtest, ytrain, ytest = train_test_split(X, Y)
+## fits NB on training data
+bayes = MultinomialNB().fit(xtrain, ytrain)
+
+## Predict and score accuracy
+print "Accuracy: %0.2f%%" % (100 * bayes.score(xtest, ytest))
+
+
